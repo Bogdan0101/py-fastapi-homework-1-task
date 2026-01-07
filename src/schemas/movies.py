533ac3b96c55705834bfb50409e1ffc45dc45606
unknown <kononovb777@gmail.com>
2026-01-07 18:02:17 +0200

    Solution#1

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,29 @@
-# Write your code here
+from typing import Optional, List
+from pydantic import BaseModel, ConfigDict
+from datetime import date
+
+
+class MovieDetailResponseSchema(BaseModel):
+    id: int
+    name: str
+    date: Optional[date]
+    score: float
+    genre: str
+    overview: Optional[str]
+    crew: str
+    orig_title: str
+    status: str
+    orig_lang: str
+    budget: float
+    revenue: float
+    country: str
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class MovieListResponseSchema(BaseModel):
+    movies: List[MovieDetailResponseSchema]
+    prev_page: Optional[str]
+    next_page: Optional[str]
+    total_pages: int
+    total_items: int
